04c05bdc13ee702281aca4eabc51676ba34ebea1
Ian Egner <ian.egner@apc-overnight.com>
2017-03-29 17:26:18 +0100

    Remove unused mkdirp

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,6 @@
 'use strict'
 const Generator = require('yeoman-generator')
 const path = require('path')
-const mkdirp = require('mkdirp')
 const slugify = require('slugify')
 
 module.exports = class extends Generator {
